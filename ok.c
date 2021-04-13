0cbdc2ee5cd698a29c1510c75f2f0b52be1603e5
Ayush Moghe <74037132+Ayush2007A@users.noreply.github.com>
2021-04-13 19:58:29 +0530

    Add files via upload

---
+++ ok.c
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  while(true){
+  int var=10;
+  std::string ota;
+  std::string x;
+  std::string y;
+  y="ben ";
+  ota="hello\n";
+  std::cout<<y;
+  std::cout<<var;
+std::cout<<"\nhello: ";
+std::cin>>x;
+if(x=="hello"){
+  std::cout<<"hello\n\n";
+}
+else{
+  std::cout<<"gande bachche\n";
+}
+  }
+}
